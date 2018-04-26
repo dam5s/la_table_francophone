c66c35c0654356462fa70d15939aee5f1c8d7ade
Damien Le Berrigaud <damien@itsdamo.com>
2018-04-26 09:19:45 -0600

    Replace Rails with simple gradle build

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-LaTableFrancophone::Application.config.secret_token = '343ef7045b9f21473e7d9c724b28d09ae085ff2d59fb6c3d808bac5270dccf915d8a7a8403fb48e94308e3fa5e9a6a750f70c87c29eceada20be47a0d8964ce2'
